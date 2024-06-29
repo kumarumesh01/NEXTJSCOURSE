0ce12bdb22620c25547d2e745eeabb787440cece
kumarumesh01 <umesh@goodeducator.com>
2024-06-29 19:26:10 +0530

    Updated

--- panda/components/Navbar.tsx
+++ panda/components/Navbar.tsx
@@ -1,26 +1,62 @@
 import { useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from '../styles/Home.module.css';
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
 
-    return (
-        <nav className={styles.nav}>
-          <button onClick={() => setIsOpen(!isOpen)} className="text-black">
-            {/* Icon or text for menu toggle */}
-            Menu
-          </button>
-          <div className={`${styles.navLinks} ${isOpen ? 'block' : 'hidden'}`}>
-            <Link href="/" passHref>
-              <span className="cursor-pointer text-white hover:underline">Home</span>
-            </Link>
-            <Link href="/about" passHref>
-              <span className="cursor-pointer text-white hover:underline">About</span>
-            </Link>
-          </div>
-        </nav>
-    )
-}
+  const handleNavigation = (e, href) => {
+    e.preventDefault();
+    const [path, hash] = href.split('#');
+    if (path === router.pathname) {
+      // Same page navigation
+      document.getElementById(hash).scrollIntoView({ behavior: 'smooth' });
+    } else {
+      // Different page navigation
+      router.push(href).then(() => {
+        if (hash) {
+          document.getElementById(hash).scrollIntoView({ behavior: 'smooth' });
+        }
+      });
+    }
+  };
+
+  const navbarHeight = isOpen ? 'h-40' : 'h-16'; // Example heights, adjust as necessary
+
+  return (
+    <nav className={`${styles.nav} ${navbarHeight} bg-black text-white flex items-center justify-between`}>
+      <button onClick={() => setIsOpen(!isOpen)} className="p-4">
+        {/* Icon or text for menu toggle */}
+        Menu
+      </button>
+      <div className={`${styles.navLinks} ${isOpen ? 'flex' : 'hidden'} flex-col md:flex-row items-center`}>
+        <Link href="/" passHref>
+          <span className="cursor-pointer hover:underline p-4">Home</span>
+        </Link>
+        <Link href="/about" passHref>
+          <span className="cursor-pointer hover:underline p-4">About</span>
+        </Link>
+        <a href="/" onClick={(e) => handleNavigation(e, '/#projects')} className="cursor-pointer hover:underline p-4">
+          Projects
+        </a>
+        <a href="/" onClick={(e) => handleNavigation(e, '/#internships')} className="cursor-pointer hover:underline p-4">
+          Internships
+        </a>
+
+        <a href="/" onClick={(e) => handleNavigation(e, '/#contacts')} className="cursor-pointer hover:underline p-4">
+          Contacts
+        </a>
+
+        <a href="/" onClick={(e) => handleNavigation(e, '/#aboutme')} className="cursor-pointer hover:underline p-4">
+          About me
+        </a>
+
+
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- panda/pages/index.tsx
+++ panda/pages/index.tsx
@@ -1,3 +1,4 @@
+// src/pages/index.tsx
 import React from 'react';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
@@ -16,15 +17,61 @@ const HomePage = () => {
           <p className="text-2xl font-bold">Hello,</p>
           <p className="text-4xl font-bold">I'm Umesh Kumar,</p>
           <p className="text-2xl font-bold">B.Tech in Electrical Engineering from IIT Delhi.</p>
-          <p className="text-2xl font-bold text-black ">Software Developer | Embedded Engineer.</p>
+          <p className="text-2xl font-bold text-black">Software Developer | Embedded Engineer.</p>
         </div>
-        <img src="/photo_umesh.jpg" alt="Umesh's Photo" className="h-15 w-15 rounded-full cursor-pointer ml-auto" style={{ paddingRight: '1rem' }} />
+        <img src="/photo_umesh.jpg" alt="Umesh's Photo" className="h-34 w-34 rounded-full cursor-pointer ml-auto relative" style={{ paddingRight: '1rem', zIndex: 10 }} />
       </div>
       {/* Main content section */}
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to My Website</h1>
-        {/* <img src="/graph_icon.png" alt="Graph Icon" className="w-24 h-24 mt-4" /> */}
-      </main>
+  
+      <div id="projects" className="w-full bg-black text-white py-10 flex flex-col items-center">
+        <h2 className="text-3xl font-bold text-red-600 text-center mb-10">Projects</h2>
+        <div className="grid grid-cols-4 gap-8 mx-auto max-w-6xl p-8">  {/* Adjusted max-width and added padding */}
+        <div className="w-64  bg-purple-700 flex items-center justify-center p-4 rounded-lg" style={{ boxShadow: '0 10px 10px rgba(128, 128, 128, 1)' }}>Project 1</div>
+          <div className="w-64 h-64 bg-purple-700 flex items-center justify-center p-4 rounded-lg" style={{ boxShadow: '0 10px 10px rgba(128, 128, 128, 1)' }}>Project 2</div>
+          <div className="w-64 h-64 bg-purple-700 flex items-center justify-center p-4 rounded-lg" style={{ boxShadow: '0 10px 10px rgba(128, 128, 128, 1)' }}>Project 3</div>
+          <div className="w-64 h-64 bg-purple-700 flex items-center justify-center p-4 rounded-lg" style={{ boxShadow: '0 10px 10px rgba(128, 128, 128, 1)' }}>Project 4</div>
+          <div className="w-64 h-64 bg-purple-700 flex items-center justify-center p-4 rounded-lg" style={{ boxShadow: '0 10px 10px rgba(128, 128, 128, 1)' }}>Project 5</div>
+          <div className="w-64 h-64 bg-purple-700 flex items-center justify-center p-4 rounded-lg" style={{ boxShadow: '0 10px 10px rgba(128, 128, 128, 1)' }}>Project 1</div>
+          <div className="w-64 h-64 bg-purple-700 flex items-center justify-center p-4 rounded-lg" style={{ boxShadow: '0 10px 10px rgba(128, 128, 128, 1)' }}>Project 2</div>
+          <div className="w-64 h-64 bg-purple-700 flex items-center justify-center p-4 rounded-lg" style={{ boxShadow: '0 10px 10px rgba(128, 128, 128, 1)' }}>Project 3</div>
+        </div>
+      </div>
+
+
+      <div id="internships" className="w-full bg-purple-700 text-white py-10 flex flex-col items-center">
+        <h2 className="text-3xl font-bold text-red-600 text-center mb-10">Internships</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl p-8">  {/* Adjusted max-width and added padding */}
+          <div className="w-64 bg-cyan-700 flex items-center justify-center p-4 rounded-lg" style={{ height: '18.5rem', boxShadow: '0 10px 10px rgba(128, 128, 128, 1)' }}>Intern 1</div>
+          <div className="w-64  bg-cyan-700 flex items-center justify-center p-4 rounded-lg" style={{ boxShadow: '0 10px 10px rgba(128, 128, 128, 1)' }}>Project 2</div>
+          <div className="w-64  bg-cyan-700 flex items-center justify-center p-4 rounded-lg" style={{ boxShadow: '0 10px 10px rgba(128, 128, 128, 1)' }}>Project 3</div>
+        </div>
+      </div>
+
+      <div id="contacts" className="w-full bg-black text-white py-10 flex flex-col items-center">
+        <h2 className="text-3xl font-bold text-red-600 text-center mb-10">Contacts</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl p-8">  {/* Adjusted max-width and added padding */}
+          <div className="w-64 bg-cyan-700 flex items-center justify-center p-4 rounded-lg" style={{ height: '18.5rem', boxShadow: '0 10px 10px rgba(128, 128, 128, 1)' }}>Intern 1</div>
+          <div className="w-64  bg-cyan-700 flex items-center justify-center p-4 rounded-lg" style={{ boxShadow: '0 10px 10px rgba(128, 128, 128, 1)' }}>Project 2</div>
+          <div className="w-64  bg-cyan-700 flex items-center justify-center p-4 rounded-lg" style={{ boxShadow: '0 10px 10px rgba(128, 128, 128, 1)' }}>Project 3</div>
+        </div>
+      </div>
+
+      <div id="aboutme" className="w-full bg-black text-white py-10 flex flex-col items-center">
+        <h2 className="text-3xl font-bold text-red-600 text-center mb-10">About me</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl p-8">  {/* Adjusted max-width and added padding */}
+          <div className="w-64 bg-cyan-700 flex items-center justify-center p-4 rounded-lg" style={{ height: '18.5rem', boxShadow: '0 10px 10px rgba(128, 128, 128, 1)' }}>Intern 1</div>
+          <div className="w-64  bg-cyan-700 flex items-center justify-center p-4 rounded-lg" style={{ boxShadow: '0 10px 10px rgba(128, 128, 128, 1)' }}>Project 2</div>
+          <div className="w-64  bg-cyan-700 flex items-center justify-center p-4 rounded-lg" style={{ boxShadow: '0 10px 10px rgba(128, 128, 128, 1)' }}>Project 3</div>
+        </div>
+      </div>
+
+
+      
+
+
+
+
+        
     </div>
   );
 };
